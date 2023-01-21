2a11bd66eabaa49367dce264095404a3d7b2d148
Sergiy Khomenko <Sergiy.Khomenko@fuib.com>
2023-01-21 17:27:10 +0200

    добавить реализацию репозитория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Store.Memory
+{
+	public class BookRepository : IBookRepository
+	{
+		private readonly Book[] books = new[]
+		{
+			new Book(1, "Art Of Programing"),
+			new Book(2, "Refactoring"),
+			new Book(3, "C Programing Language")
+		};
+		public Book[] GetByTitle( string titlePart ) {
+			return books.Where(book => book.Title.Contains(titlePart))
+				.ToArray();
+		}
+	}
+}
