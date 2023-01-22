ce5aca55b5c1ba31825aee038a301ff4ca604d87
Sergiy Khomenko <Sergiy.Khomenko@fuib.com>
2023-01-22 19:17:00 +0200

    добавить уровень пресентации

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Memory;
+
+namespace Store.Web.Controllers
+{
+	public class SearchController : Controller
+	{
+		private readonly IBookRepository bookRepository;
+
+		public SearchController( ) {
+			this.bookRepository = new BookRepository();
+		}
+
+ 		public IActionResult Index(string query) {
+			var books = bookRepository.GetByTitle( query );
+
+			return View( books );
+		}
+	}
+}
