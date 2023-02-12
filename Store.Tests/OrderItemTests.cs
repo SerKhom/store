de29cc6f8b4dc577931f8856ef86e4f5335614b7
SerKhom <sergii.khomenko893@gmail.com>
2023-02-12 17:21:13 +0200

    упростить кодзаказа

--- Store.Tests/OrderItemTests.cs
+++ Store.Tests/OrderItemTests.cs
@@ -34,5 +34,66 @@ namespace Store.Tests
             Assert.Equal(2, orderItem.Count);
             Assert.Equal(3m, orderItem.Price);
         }
+
+        [Fact]
+        public void Count_WithNegativeValue_ThrowArgumentOutOfRangeException()
+        {
+            var orderItem = new OrderItem(0, 5, 0m);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                orderItem.Count = -1;
+            });
+        }
+
+        [Fact]
+        public void Count_WithZeroValue_ThrowArgumentOutOfRangeException()
+        {
+            var orderItem = new OrderItem(0, 5, 0m);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                orderItem.Count = 0;
+            });
+        }
+
+        [Fact]
+        public void Count_WithPositiveValue_SetsValue()
+        {
+            var orderItem = new OrderItem(0, 5, 0m);
+
+            orderItem.Count = 10;
+
+            Assert.Equal(10, orderItem.Count);
+        }
+
+        [Fact]
+        public void GetItem_WithExistingItem_ReturnsItem()
+        {
+            var order = new Order(1, new[]
+            {
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m)
+            });
+
+            var orderItem = order.GetItem(1);
+
+            Assert.Equal(3, orderItem.Count);
+        }
+
+        [Fact]
+        public void GetItem_WithNonExistingItem_ThrowsInvalidOperationException()
+        {
+            var order = new Order(1, new[]
+            {
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m)
+            });
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                order.GetItem(100);
+            });
+        }
     }
 }
